b77faf82b5f2b510b284e59bcd02debe8304656a
tushar upadhyay <tusharupadhyay690@gmail.com>
2024-09-23 17:30:44 +0530

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,11 +11,11 @@ export default {
       padding: "15px",
     },
     screens: {
-      sm: "640px",
-      md: "768px",
-      lg: "960px",
-      xl: "1200px",
-      '2xl': "1440px",
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
     },
     extend: {
       borderRadius: {
